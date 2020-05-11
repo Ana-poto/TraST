a910afe4b7bf0d30262d04664666c6e317f441bf
CotiugaIlie <61407980+CotiugaIlie@users.noreply.github.com>
2020-05-11 20:28:59 +0300

    Add files via upload

--- TraST_PHP/Auth/ProfileD.php
+++ TraST_PHP/Auth/ProfileD.php
@@ -1,1 +1,48 @@
 <?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=trast", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $firstName = $_POST['firstName'];
+
+    $lastName = $_POST['lastName'];
+
+    $nickname = $_POST['nickname'];
+
+    $hobby = $_POST['hobby'];
+
+    $age = $_POST['age'];
+
+    $stmt = $conn->prepare("SELECT username FROM users where username= :username ");
+    $stmt->bindParam(':username',$_SESSION['username']);
+    $stmt->execute();
+
+    if($stmt->rowCount()===0){
+        echo ("Username or password are wrong.");
+
+    }
+    else{
+        $stmt = $conn->prepare("update users  set first_name=:firstName,last_name=:lastName,nickname=:nickname,hobbys=:hobby,age=:age where username=:username");
+        $stmt->bindParam(':username',$_SESSION['username']);
+        $stmt->bindParam(':firstName', $firstName);
+        $stmt->bindParam(':lastName', $lastName);
+        $stmt->bindParam(':nickname', $nickname);
+        $stmt->bindParam(':hobby', $hobby);
+        $stmt->bindParam(':age', $age);
+        $stmt->execute();
+        header('location:../Profile.php');
+    }
+
+
+}
+catch(PDOException $e)
+{
+    echo "Error: " . $e->getMessage();
+}
+$conn=null;
+?>
---
+++ TraST_PHP/Auth/ProfileEmailChange.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=trast", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $password = $_POST['pass'];
+
+    $new_email = $_POST['new_email'];
+
+    $email = $_POST['e-mail'];
+
+    $stmt = $conn->prepare("SELECT username FROM users where username= :username  and email=:email and password=:pass");
+    $stmt->bindParam(':username',$_SESSION['username']);
+    $stmt->bindParam(':pass',$password);
+    $stmt->bindParam(':email',$email);
+    $stmt->execute();
+
+    if($stmt->rowCount()===0){
+        echo ("Email or pass do not match.");
+
+    }
+    else{
+        if (strlen($new_email) <16){ /** trebuie amanuntit **/
+            echo ("The new email isn't okay.");
+        }
+        else {
+            $stmt = $conn->prepare("update users  set email=:email where username=:username");
+            $stmt->bindParam(':username', $_SESSION['username']);
+            $stmt->bindParam(':email', $email_pass);
+            $stmt->execute();
+            header('location:../Profile.php');
+        }
+    }
+
+
+}
+catch(PDOException $e)
+{
+    echo "Error: " . $e->getMessage();
+}
+$conn=null;
+?>
